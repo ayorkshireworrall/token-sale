f721904bfb74757351bb9649dcfab8c5d2acb8b7
ayorkshireworrall <a.worrall2512@googlemail.com>
2023-01-20 15:48:49 +0000

    Basic structure and escrow init

--- programs/token-sale/src/lib.rs
+++ programs/token-sale/src/lib.rs
@@ -6,10 +6,86 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod token_sale {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    /// Used by the admin user to create an escrow account holding tokens
+    pub fn initialize(ctx: Context<Initialize>, name: String, rate: u64, supply: u64) -> Result<()> {
+        // TODO check that user's account has at least as many tokens as in the supply
+        
+        let escrow = &mut ctx.accounts.escrow;
+        let bump = *ctx.bumps.get("escrow").ok_or(TokenSaleError::CannotGetEscrowBump)?;
+
+        escrow.admin = *ctx.accounts.user.key;
+        escrow.name = name;
+        escrow.total_token_availability = supply;
+        escrow.exchange_rate = rate;
+        escrow.bump = bump;
+        Ok(())
+    }
+
+    /// Used by the admin user to cancel an existing escrow account holding tokens
+    pub fn cancel(ctx: Context<Cancel>, _name: String) -> Result<()> {
+        Ok(())
+    }
+
+    /// Used by the end user to send SOL to the escrow account and retrieve tokens
+    pub fn exchange(ctx: Context<Exchange>, _name: String) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct Initialize<'info> {
+    #[account(
+        init, 
+        payer=user, 
+        space=9000, 
+        seeds=[b"escrow"],
+        bump
+    )]
+    escrow: Account<'info, EscrowAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>, 
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Cancel<'info> {
+    #[account(
+        mut,
+        seeds=[b"escrow"],
+        bump
+    )]
+    escrow: Account<'info, EscrowAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Exchange<'info> {
+    #[account(
+        mut,
+        seeds=[b"escrow"],
+        bump
+    )]
+    escrow: Account<'info, EscrowAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    system_program: Program<'info, System>, 
+}
+
+#[account]
+pub struct EscrowAccount {
+    pub admin: Pubkey,
+    pub name: String,
+    pub exchange_rate: u64, // the number of tokens that can be bought for 1 SOL
+    pub total_token_availability: u64,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum TokenSaleError {
+    #[msg("Unable to get escrow bump")]
+    CannotGetEscrowBump,
+}
--- tests/token-sale.ts
+++ tests/token-sale.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { BN } from "bn.js";
+import * as assert from "assert"
+const { SystemProgram } = anchor.web3;
 import { TokenSale } from "../target/types/token_sale";
 
 describe("token-sale", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.TokenSale as Program<TokenSale>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const name = 'Test Escrow'
+    const rate = new BN(100)
+    const supply = new BN(1000000)
+    const [escrowAccountPubkey] = anchor.web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode('escrow')],
+      program.programId
+    );
+    await program.methods
+      .initialize(name, rate, supply)
+      .accounts({
+        escrow: escrowAccountPubkey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .rpc();
+
+    const escrowAccount = await program.account.escrowAccount.fetch(escrowAccountPubkey);
+    assert.ok(escrowAccount.name === name);
+    assert.ok(escrowAccount.exchangeRate.eq(rate));
+    assert.ok(escrowAccount.totalTokenAvailability.eq(supply));
+    assert.ok(escrowAccount.admin.equals(provider.wallet.publicKey));
   });
 });
