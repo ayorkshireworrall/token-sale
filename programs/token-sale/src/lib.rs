fc94d77e37dfff6ce2a4a4a90cae640fe16eee01
ayorkshireworrall <a.worrall2512@googlemail.com>
2023-01-23 18:33:28 +0000

    Add token account to init process

--- programs/token-sale/src/lib.rs
+++ programs/token-sale/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount, Token};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -15,9 +16,11 @@ pub mod token_sale {
 
         escrow.admin = *ctx.accounts.user.key;
         escrow.name = name;
+        escrow.token_holder = *ctx.accounts.token_holder.to_account_info().key;
         escrow.total_token_availability = supply;
         escrow.exchange_rate = rate;
         escrow.bump = bump;
+
         Ok(())
     }
 
@@ -43,9 +46,20 @@ pub struct Initialize<'info> {
         bump
     )]
     escrow: Account<'info, EscrowAccount>,
+    mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [b"token_holder".as_ref()],
+        bump,
+        payer = user,
+        token::mint = mint,
+        token::authority = user,
+    )]
+    token_holder: Account<'info, TokenAccount>,
     #[account(mut)]
     user: Signer<'info>,
     system_program: Program<'info, System>, 
+    token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
@@ -77,7 +91,8 @@ pub struct Exchange<'info> {
 
 #[account]
 pub struct EscrowAccount {
-    pub admin: Pubkey,
+    pub admin: Pubkey, // the user that created the token sale
+    pub token_holder: Pubkey, // the address created to hold the tokens for sale
     pub name: String,
     pub exchange_rate: u64, // the number of tokens that can be bought for 1 SOL
     pub total_token_availability: u64,
--- tests/token-sale.ts
+++ tests/token-sale.ts
@@ -1,29 +1,71 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { BN } from "bn.js";
+import { BN, min } from "bn.js";
 import * as assert from "assert"
-const { SystemProgram } = anchor.web3;
 import { TokenSale } from "../target/types/token_sale";
+import { PublicKey } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, createMint, createAccount, mintTo, getAccount } from "@solana/spl-token";
+const { SystemProgram } = anchor.web3;
 
 describe("token-sale", () => {
+
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.TokenSale as Program<TokenSale>;
 
+  let mint = null as PublicKey;
+  let adminTokenAccount = null as PublicKey;
+
+  // Required Keys
+  const admin = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+
+  // Known seeds
+  const escrowSeed = 'escrow';
+  const encodedEscrowSeed = [anchor.utils.bytes.utf8.encode(escrowSeed)];
+  const tokenHolderSeed = 'token_holder';
+  const encodedTokenHolderSeed = [anchor.utils.bytes.utf8.encode(tokenHolderSeed)];
+
+  // PDAs
+  const [escrowAccountPubkey] = anchor.web3.PublicKey.findProgramAddressSync(encodedEscrowSeed, program.programId);
+  const [tokenHolderPubkey] = anchor.web3.PublicKey.findProgramAddressSync(encodedTokenHolderSeed, program.programId);
+
+
   it("Is initialized!", async () => {
-    const name = 'Test Escrow'
-    const rate = new BN(100)
-    const supply = new BN(1000000)
-    const [escrowAccountPubkey] = anchor.web3.PublicKey.findProgramAddressSync(
-      [anchor.utils.bytes.utf8.encode('escrow')],
-      program.programId
+    const name = 'Test Escrow';
+    const rate = new BN(100);
+    const supply = 1000000
+    const supplyBN = new BN(supply);
+
+    // airdrop SOL to admin who will be paying
+    const signature = await provider.connection.requestAirdrop(admin.publicKey, 1000000000);
+    const latestBlockhash = await provider.connection.getLatestBlockhash();
+    await provider.connection.confirmTransaction(
+      {
+        signature,
+        ...latestBlockhash,
+      },
+      'confirmed'
     );
+
+    // dummy token mint
+    mint = await createMint(provider.connection, admin, mintAuthority.publicKey, null, 0);
+
+    // create a token account for admin so they can hold the token minted above
+    adminTokenAccount = await createAccount(provider.connection, admin, mint, admin.publicKey);
+
+    // mint dummy token to admin account
+    await mintTo(provider.connection, admin, mint, adminTokenAccount, mintAuthority, supply);
+
+
     await program.methods
-      .initialize(name, rate, supply)
+      .initialize(name, rate, supplyBN)
       .accounts({
         escrow: escrowAccountPubkey,
+        tokenHolder: tokenHolderPubkey,
+        mint: mint,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId
       })
@@ -32,7 +74,7 @@ describe("token-sale", () => {
     const escrowAccount = await program.account.escrowAccount.fetch(escrowAccountPubkey);
     assert.ok(escrowAccount.name === name);
     assert.ok(escrowAccount.exchangeRate.eq(rate));
-    assert.ok(escrowAccount.totalTokenAvailability.eq(supply));
+    assert.ok(escrowAccount.totalTokenAvailability.eq(supplyBN));
     assert.ok(escrowAccount.admin.equals(provider.wallet.publicKey));
   });
 });
